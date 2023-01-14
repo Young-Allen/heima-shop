705db6ec73762e27ccf771ad15d6f7e424cac6bd
Gogs <gogs@fake.local>
2023-01-14 13:57:58 +0800

    完成了 home 首页的开发

--- main.js
+++ main.js
@@ -2,6 +2,35 @@
 // #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
+// 按需导入 $http 对象
+import { $http } from '@escook/request-miniprogram'
+
+// 在 uni-app 项目中，可以把 $http 挂载到 uni 顶级对象之上，方便全局调用
+uni.$http = $http
+
+//请求根路径
+$http.baseUrl = 'http://www.uinav.com'
+
+// 封装的展示消息提示的方法
+uni.$showMsg = function (title = '数据加载失败！', duration = 1500) {
+  uni.showToast({
+    title,
+    duration,
+    icon: 'none',
+  })
+}
+
+//请求拦截器
+$http.beforeRequest = function(options){
+  uni.showLoading({
+    title: "数据加载中..."
+  })
+}
+
+//响应拦截器
+$http.afterRequest = function(){
+  uni.hideLoading()
+}
 
 Vue.config.productionTip = false
 
